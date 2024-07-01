abc4714659b566538b24c0a60440ef61a57af69b
Thakisis <sub722@gmail.com>
2024-07-01 23:33:46 +0200

    change read dropoff to client

--- server/Queries/getCities.js
+++ server/Queries/getCities.js
@@ -23,14 +23,12 @@ export async function getCities() {
 }
 export async function getCitiesDropOff(origen) {
     //await sleep(3000)
-    const realFilepath = path.join(process.cwd(), 'public', "ciudades.json")
-    const file= fs.readFile(realFilepath,'utf8')
-    const data = JSON.parse(file);
-    //const response = await fetch(`https://carefast.vercel.app/ciudades.json`)
-    //if (!response.ok) {
-      //  return { codeError: response.status }
-    //}
-    //const data = await response.json()
+    
+    const response = await fetch(`https://carefast.vercel.app/ciudades.json`)
+    if (!response.ok) {
+        return { codeError: response.status }
+    }
+    const data = await response.json()
     return data.filter(ciudad => ciudad.startsWith(origen[0]))
 
 }
